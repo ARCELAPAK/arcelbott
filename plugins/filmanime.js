aab765a12824fc5d45e4396cc351187b8c99ae05
ARCELAPAK <93075823+ARCELAPAK@users.noreply.github.com>
2021-10-30 12:28:17 +0700

    Update filmanime.js

--- plugins/filmanime.js
+++ plugins/filmanime.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, text }) => {
 
     if (!text) return conn.reply(m.chat, 'Harap Masukan Nama Film Animenya', m)
 
-	axios.get(`https://arugaz.herokuapp.com/api/sdmovie?film=${text}`).then ((res) => {
+	axios.get(`https://api.caliph71.xyz/api/anidsr?query=text&apikey=6N8tIK95`).then ((res) => {
 	 	let hasil = `*Film Anime ${text}*\n\nJudul : ${res.data.result.title}\nRating : ${res.data.result.rating}\nInfo : ${res.data.result.sinopsis}\nLink Video : ${res.data.result.video}`
 
     conn.reply(m.chat, hasil, m)
