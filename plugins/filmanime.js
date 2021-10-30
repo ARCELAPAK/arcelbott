f60873c2e7f2683dcd72d920d92dd03a8df9935f
ARCELAPAK <93075823+ARCELAPAK@users.noreply.github.com>
2021-10-30 12:40:07 +0700

    Update filmanime.js

--- plugins/filmanime.js
+++ plugins/filmanime.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, 'Harap Masukan Nama Film Animenya', m)
 
 	axios.get(`https://api.caliph71.xyz/api/anidsr?query=text&apikey=6N8tIK95`).then ((res) => {
-	 	let hasil = `*Film Anime ${text}*\n\nJudul : ${name}\nRating : ${star}\nInfo : ${desc}\nLink Video : ${url}`
+	 	let hasil = `**Film Anime ${text}*\n\nJudul : ${res.data.result.name}\nRating : ${res.data.result.stars}\nInfo : ${res.data.result.desc}\nLink Video : ${res.data.result.url}`
 
     conn.reply(m.chat, hasil, m)
 	})
